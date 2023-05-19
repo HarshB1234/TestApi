835e203225f31cb9f3e531986d96a511fcb93153
Harsh Bhayani <harshbhayani05@gmail.com>
2023-05-19 12:06:28 +0530

    delete account operation

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const register_router = require("./routes/register");
 const login_router = require("./routes/login");
 const logout_router = require("./routes/logout");
 const forgetpass_router = require("./routes/forgetpassword");
+const deleteacc_router = require("./routes/deleteaccount");
 
 const app = express();
 
@@ -22,5 +23,6 @@ app.use("/register", register_router);
 app.use("/login", login_router);
 app.use("/logout", logout_router);
 app.use("/forgetpassword", forgetpass_router);
+app.use("/deleteacc", deleteacc_router);
 
 app.listen(port, () => console.log(`Listening at port ${port}`));
--- src/routes/forgetpassword.js
+++ src/routes/forgetpassword.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require('../src/middleware/auth');
+const auth = require('../middleware/auth');
 const {emailid, otp, resetPass} = require("../controllers/forgetpassword")
 
 router.route("/").post(emailid);
