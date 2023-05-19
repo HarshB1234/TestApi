22505145c3152a736ef17a68a1d1f88c0479cfd7
Harsh Bhayani <harshbhayani05@gmail.com>
2023-05-19 10:09:48 +0530

    forget password operation

--- src/routes/forgetpassword.js
+++ src/routes/forgetpassword.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// const auth = require('../src/middleware/auth');
+const auth = require('../src/middleware/auth');
 const {emailid, otp, resetPass} = require("../controllers/forgetpassword")
 
 router.route("/").post(emailid);
-// router.route("/:email").post(auth, otp);
-router.route("/reset/:email").post(resetPass);
+router.route("/:email").post(auth, otp);
+router.route("/reset/:email").post(auth, resetPass);
 
 module.exports = router
